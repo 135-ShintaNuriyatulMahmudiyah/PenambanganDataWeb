5b5dca6baa49234510de262ca9eae145379509a9
135-ShintaNuriyatulMahmudiyah <112710815+135-ShintaNuriyatulMahmudiyah@users.noreply.github.com>
2022-11-28 08:29:34 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -31,76 +31,7 @@ with tab2:
 
    
 
-    # Mengambil kolom Rata-rata overdue dan mentranformasi menggunakan one-hot encoding
-    rata_overdue = pd.get_dummies(X["rata_rata_overdue"], prefix="overdue")
-    X = X.join(rata_overdue)
-
-    X = X.drop(columns="rata_rata_overdue")
-
-    labels = data["race/ethnicity"]
-    # 
-    KPR_status = pd.get_dummies(X["kpr_aktif"], prefix="KPR")
-    X = X.join(KPR_status)
-
-    # remove "rata_rata_overdue" feature
-    X = X.drop(columns = "gender")
-
-    st.write("Menampilkan dataframe yang rata-rata overdue, risk rating dan kpr aktif sudah di drop")
-    st.dataframe(X)
-
-    st.write(" ## Normalisasi")
-    st.write("Normalize feature 'pendapatan_setahun_juta', 'durasi_pinjaman_bulan', 'jumlah_tanggungan'")
-    old_normalize_feature_labels = ['pendapatan_setahun_juta', 'durasi_pinjaman_bulan', 'jumlah_tanggungan']
-    new_normalized_feature_labels = ['norm_pendapatan_setahun_juta', 'norm_durasi_pinjaman_bulan', 'norm_jumlah_tanggungan']
-    normalize_feature = data[old_normalize_feature_labels]
-
-    st.dataframe(normalize_feature)
-
-    scaler = MinMaxScaler()
-
-    scaler.fit(normalize_feature)
-
-    normalized_feature = scaler.transform(normalize_feature)
-
-    normalized_feature_df = pd.DataFrame(normalized_feature, columns = new_normalized_feature_labels)
-
-    st.write("Data setelah dinormalisasi")
-    st.dataframe(normalized_feature_df)
-
-    X = X.drop(columns = old_normalize_feature_labels)
-
-    X = X.join(normalized_feature_df)
-
-    X = X.join(labels)
-
-    st.write("Dataframe X baru")
-    st.dataframe(X)
-
-    subject_lables = ["Unnamed: 0",  "kode_kontrak"]
-    X = X.drop(columns = subject_lables)
-
-    # percent_amount_of_test_data = / HUNDRED_PERCENT
-    percent_amount_of_test_data = 0.3
-
-    st.write("Dataframe X baru yang tidak ada fitur/kolom unnamed: 0 dan kode kontrak")
-    st.dataframe(X)
-    st.write("## Hitung Data")
-    st.write("- Pisahkan kolom risk rating dari data frame")
-    st.write("- Ambil kolom 'risk rating' sebagai target kolom untuk kategori kelas")
-    st.write("- Pisahkan data latih dengan data tes")
-    st.write("""            Spliting Data
-
-                data latih (nilai data)
-                X_train 
-
-                data tes (nilai data)
-                X_test 
-
-                data latih (kelas data)
-                y_train
-
-                data tes (kelas data)
-                y_test""")
+   
 
     # separate target 
 
