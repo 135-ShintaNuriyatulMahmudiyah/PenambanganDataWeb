de0544d3e4d2b622f224f1ee143b7a166c9ebde1
135-ShintaNuriyatulMahmudiyah <112710815+135-ShintaNuriyatulMahmudiyah@users.noreply.github.com>
2022-11-30 15:56:36 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -1,17 +1,23 @@
 import streamlit as st
-import numpy as np
 import pandas as pd
+import numpy as np
+from sklearn import preprocessing
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from collections import OrderedDict
-from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import confusion_matrix, accuracy_score, recall_score, precision_score, f1_score
 from sklearn.neighbors import KNeighborsClassifier
+from numpy import array
+from sklearn import tree
+from sklearn.naive_bayes import GaussianNB
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix, accuracy_score, recall_score, precision_score, f1_score
 from sklearn.tree import DecisionTreeClassifier
+from collections import OrderedDict
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import BaggingClassifier
 from sklearn.datasets import make_classification
 from sklearn.svm import SVC
+import altair as alt
+from sklearn.utils.validation import joblib
 
 st.write(""" 
 # Penambangan Data Web
@@ -32,7 +38,7 @@ with tab1:
         df = pd.read_table(uploaded_file)
         st.write("Nama File Anda = ", uploaded_file.name)
         st.dataframe(df)
-with tab2:
+
   
    
 
