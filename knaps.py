5e9096351919057c59eb88287d7a27a7759833ea
135-ShintaNuriyatulMahmudiyah <112710815+135-ShintaNuriyatulMahmudiyah@users.noreply.github.com>
2022-11-30 17:50:29 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -53,9 +53,7 @@ with tab2:
     x_scaled= scaler.fit_transform(x)
     x_scaled
     
-    x_train, x_test,y_train,y_test= train_test_split(x,y,random_state=0)
-    x_train_scaled, x_test_scaled,y_train_scaled,y_test_scaled= train_test_split(x_scaled,y,random_state=0)
-  
+
    
 
    
